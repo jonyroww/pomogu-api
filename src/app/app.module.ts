6d88ea587ff260e4844c530ee1b332fa695ee9c1
Nikita <jonyroww@gmail.com>
2020-02-19 18:53:48 +0300

    MailerModule in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,20 @@ import { ConfigModule } from "./../config/config.module";
 import { VolunteerRequestsModule } from "../volunteer-requests/volunteer-requests.module";
 import { PhotosModule } from "../photos/photos.module";
 import { AuthModule } from "../auth/auth.module";
+import { HandlebarsAdapter, MailerModule } from "@nest-modules/mailer";
+import { path } from "app-root-path";
 
 @Module({
   imports: [
+    MailerModule.forRoot({
+      transport: "smtp.mail.ru:465",
+      defaults: { from: "socqr@mail.ru" },
+      template: {
+        dir: path + "templates",
+        adapter: new HandlebarsAdapter(),
+        options: { strict: true }
+      }
+    }),
     TypeOrmModule.forRoot(),
     OrganisationsModule,
     CitezenTypesModule,
