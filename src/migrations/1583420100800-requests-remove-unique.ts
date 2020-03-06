e7609f14b11de0b77d02aa531f4a32e7a038cc22
Nikita <jonyroww@gmail.com>
2020-03-06 19:37:31 +0300

    migration fix

--- src/migrations/1583420100800-requests-remove-unique.ts
+++ src/migrations/1583420100800-requests-remove-unique.ts
@@ -1,4 +1,9 @@
-import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
+import {
+  MigrationInterface,
+  QueryRunner,
+  TableColumn,
+  TableIndex
+} from "typeorm";
 
 export class requestsRemoveUnique1583420100800 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<any> {
@@ -12,12 +17,20 @@ export class requestsRemoveUnique1583420100800 implements MigrationInterface {
   }
 
   public async down(queryRunner: QueryRunner): Promise<any> {
-    await queryRunner.query(
-      `ALTER TABLE "requests" ADD CONSTRAINT "UQ_4f8a7e1a3fbd1caea1b5cc5671e"  `
+    await queryRunner.createIndex(
+      "requests",
+      new TableIndex({
+        name: "UQ_requests_phone",
+        columnNames: ["phone"]
+      })
     );
 
-    await queryRunner.query(
-      `ALTER TABLE "requests" ADD CONSTRAINT "UQ_15831369ce9f4212e4c745f50aa"  `
+    await queryRunner.createIndex(
+      "requests",
+      new TableIndex({
+        name: "UQ_requests_email",
+        columnNames: ["email"]
+      })
     );
   }
 }
