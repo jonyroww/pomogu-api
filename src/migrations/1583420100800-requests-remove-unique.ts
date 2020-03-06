7549e612d4e55aa88fc328244d292e70db37c4d7
Nikita <jonyroww@gmail.com>
2020-03-06 19:25:01 +0300

    deleted constraints from requests table

--- src/migrations/1583420100800-requests-remove-unique.ts
+++ src/migrations/1583420100800-requests-remove-unique.ts
@@ -2,30 +2,22 @@ import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
 
 export class requestsRemoveUnique1583420100800 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<any> {
-    await queryRunner.changeColumn(
-      "requests",
-      "phone",
-      new TableColumn({
-        name: "phone",
-        type: "varchar",
-        isNullable: false,
-        isUnique: false
-      })
+    await queryRunner.query(
+      `ALTER TABLE "requests" DROP CONSTRAINT "UQ_4f8a7e1a3fbd1caea1b5cc5671e" `
     );
 
-    await queryRunner.changeColumn(
-      "requests",
-      "email",
-      new TableColumn({
-        name: "email",
-        type: "varchar",
-        isNullable: false,
-        isUnique: false
-      })
+    await queryRunner.query(
+      `ALTER TABLE "requests" DROP CONSTRAINT "UQ_15831369ce9f4212e4c745f50aa"  `
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<any> {
-    await queryRunner.dropTable("requests");
+    await queryRunner.query(
+      `ALTER TABLE "requests" ADD CONSTRAINT "UQ_4f8a7e1a3fbd1caea1b5cc5671e"  `
+    );
+
+    await queryRunner.query(
+      `ALTER TABLE "requests" ADD CONSTRAINT "UQ_15831369ce9f4212e4c745f50aa"  `
+    );
   }
 }
